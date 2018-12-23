c885bcae1b76a55bcff611231cdefc4e4c515661
oshairo <oosha.github@gmail.com>
2018-12-23 21:58:05 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,19 +34,19 @@
     omf install bobthefish
 
     ### powerline_fonts インストール
-    git clone https://github.com/powerline/fonts.git --depth=1 $HOME/repositories/powerline/fonts
-    sh $HOME/repositories/powerline/fonts/install.sh
+    git clone https://github.com/powerline/fonts.git --depth=1 $HOME/repos/$USER/fonts/powerline/fonts
+    sh $HOME/repos/$USER/fonts/powerline/fonts/install.sh
 
     ### config.fish シンボリックリンクを作成
-    ln -s $HOME/repositories/oosha/dotfiles/_config.fish $HOME/.config/fish/config.fish
+    ln -s $HOME/repos/$USER/dotfiles/_config.fish $HOME/.config/fish/config.fish
 
 # Vim
     ### .vimrc シンボリックリンクを作成
-    ln -s $HOME/repositories/oosha/dotfiles/_vimrc $HOME/.vimrc
+    ln -s $HOME/repos/$USER/dotfiles/_vimrc $HOME/.vimrc
 
 # git
     ### .gitconfig シンボリックリンクを作成
-    ln -s $HOME/repositories/oosha/dotfiles/_gitconfig $HOME/.gitconfig
+    ln -s $HOME/repos/$USER/dotfiles/_gitconfig $HOME/.gitconfig
 
 # nodebrew
     ### nodebrew インストール
@@ -61,6 +61,14 @@
     code --install-extension bierner.markdown-checkbox
     code --install-extension mdickin.markdown-shortcuts
     code --install-extension eamodio.gitlens
+    code --install-extension angular.ng-template
+    code --install-extension arjun.swagger-viewer
+    code --install-extension ritwickdey.liveserver
+    code --install-extension equinusocio.vsc-material-theme
+    code --install-extension isayme.vscode-prettier-standard
+    code --install-extension hookyqr.beautify
+    code --install-extension sidthesloth.html5-boilerplate
+
 
 # exit
     echo "完了"
