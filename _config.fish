05642b8808b53ced84e5765060d376ca08135a71
oshairo <oosha.github@gmail.com>
2019-10-30 15:39:45 +0900

    Update _config.fish

--- _config.fish
+++ _config.fish
@@ -9,3 +9,6 @@ eval (direnv hook fish)
 
 ## PATH_EXPORT
 export PATH=/Users/oosha/.nodebrew/current/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+## JAVA_HOME_EXPORT
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
