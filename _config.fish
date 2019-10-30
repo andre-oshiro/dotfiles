5fae697fa6d6fa816e47584cc0c97220367954e0
oshairo <oosha.github@gmail.com>
2019-10-30 16:20:19 +0900

    Update _config.fish

--- _config.fish
+++ _config.fish
@@ -14,4 +14,4 @@ export PATH=/Users/oosha/.nodebrew/current/bin:/usr/local/bin:/usr/bin:/bin:/usr
 export JAVA_HOME=(/usr/libexec/java_home -v 1.8)
 
 ## ANDROID_HOME_EXPORT
-export ANDROID_HOME=/Users/oosha/Library/Android/sdk
+export ANDROID_HOME=/usr/local/share/android-sdk
