c055a1411432bfb0ddf8a3154e3c843b6e3f79e4
oshairo <oosha.github@gmail.com>
2021-01-04 01:18:04 +0900

    Update setup-font.sh

--- scripts/setup-font.sh
+++ scripts/setup-font.sh
@@ -3,5 +3,5 @@
 echo "********************************************************************"
 echo "powerline font"
 echo "********************************************************************"
-git clone git@github.com:powerline/fonts.git --depth=1 $HOME/repos/powerline/fonts
+git clone --depth 1 git@github.com:powerline/fonts.git $HOME/repos/powerline/fonts
 sh $HOME/repos/powerline/fonts/install.sh
