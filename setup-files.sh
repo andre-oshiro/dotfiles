9cb2153fb34f3ad4a305be0a975920c05bccd6a8
oshairo <oosha.github@gmail.com>
2021-01-03 14:11:58 +0900

    Update setup-files.sh

--- setup-files.sh
+++ setup-files.sh
@@ -1,7 +1,7 @@
 echo "********************************************************************"
 echo "symbolyc links"
 echo "********************************************************************"
-ln -s $HOME/repos/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/repos/dotfiles/.zshrc $HOME/.zshrc
-ln -s $HOME/repos/dotfiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/repos/dotfiles/iterm2 $HOME/iterm2
+ln -nfs $HOME/repos/dotfiles/.vimrc $HOME/.vimrc
+ln -nfs $HOME/repos/dotfiles/.zshrc $HOME/.zshrc
+ln -nfs $HOME/repos/dotfiles/.gitconfig $HOME/.gitconfig
+ln -nfs $HOME/repos/dotfiles/iterm2 $HOME/iterm2
