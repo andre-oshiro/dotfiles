b5dde033335a2677ef7ce96a74b802682117c95d
oshairo <oosha.github@gmail.com>
2021-01-04 00:40:04 +0900

    Update setup-files.sh

--- scripts/setup-files.sh
+++ scripts/setup-files.sh
@@ -6,4 +6,5 @@ echo "********************************************************************"
 ln -nfs $HOME/repos/dotfiles/.vimrc $HOME/.vimrc
 ln -nfs $HOME/repos/dotfiles/.zshrc $HOME/.zshrc
 ln -nfs $HOME/repos/dotfiles/.gitconfig $HOME/.gitconfig
-ln -nfs $HOME/repos/dotfiles/iterm2 $HOME/iterm2
+mkdir $HOME/iterm2
+ln -nfs $HOME/repos/dotfiles/iterm2/com.googlecode.iterm2.plist $HOME/iterm2/com.googlecode.iterm2.plist
