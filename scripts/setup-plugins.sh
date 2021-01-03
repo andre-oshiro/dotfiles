08da4d3f88226b15924e71e58fd12286d0356a7a
oshairo <oosha.github@gmail.com>
2021-01-04 01:14:47 +0900

    Update setup-plugins.sh

--- scripts/setup-plugins.sh
+++ scripts/setup-plugins.sh
@@ -4,4 +4,4 @@ echo "********************************************************************"
 echo "Setup plugins"
 echo "********************************************************************"
 git clone git@github.com:agnoster/agnoster-zsh-theme.git --depth=1 $HOME/zsh-repos/themes
-git clone git@github.com:zsh-users/zsh-autosuggestions.git
+git clone git@github.com:zsh-users/zsh-autosuggestions.git --depth=1 $HOME/zsh-repos/plugins
