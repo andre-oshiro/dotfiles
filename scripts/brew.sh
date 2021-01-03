be2dc1fb24d0c9b817ebeb0e0f5220c898382e84
oshairo <oosha.github@gmail.com>
2021-01-03 14:52:28 +0900

    Update brew.sh

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -5,7 +5,6 @@ echo "brew.sh"
 echo "********************************************************************"
 brew install zsh
 brew install tree
-brew install curl
 brew install wget
 brew install direnv
 brew install peco
@@ -16,6 +15,7 @@ brew install fzf
 brew install tig
 brew install lf
 brew install nodebrew
+brew install neofetch
 brew install --cask google-chrome
 brew install --cask google-japanese-ime
 brew install --cask iterm2
